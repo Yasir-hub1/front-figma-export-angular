8dd4bf23e4d2fd7f2330a0578a64c39a6220c804
Yasir-hub1 <73040332+Yasir-hub1@users.noreply.github.com>
2025-04-13 15:27:05 -0400

    Funcional, corregir exportacion responsive, y duplicidad de usuario compartir

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
+import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { AuthProvider } from './context/AuthContext';
+import Routes from './Routes';
+import './styles/main.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <AuthProvider>
+        <Routes />
+      </AuthProvider>
+    </Router>
   );
 }
 
