27d701f3abc5823893ab40d8eca1bac553554133
Yasir-hub1 <73040332+Yasir-hub1@users.noreply.github.com>
2025-04-26 18:21:22 -0400

    Update EditorContext.js

--- src/context/EditorContext.js
+++ src/context/EditorContext.js
@@ -37,7 +37,7 @@ const [elementInteractions, setElementInteractions] = useState({});
 useEffect(() => {
   if (!id || !currentUser) return;
 
-  const newSocket = io('http://45.55.145.232:80', {
+  const newSocket = io('http://159.203.124.196:80', {
     withCredentials: true,
     transports: ['websocket'],
   });
