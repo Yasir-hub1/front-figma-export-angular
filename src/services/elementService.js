d98fdcf1e2ae07f8cc31f8f3b86ac616c3fa3df6
CLOUD <cloud6911@hotmail.com>
2025-09-28 18:11:20 -0400

    change ip

--- src/services/elementService.js
+++ src/services/elementService.js
@@ -1,7 +1,7 @@
 // src/services/elementService.js - CORREGIDO
 import axios from '../utils/axiosConfig';
 
-const API_BASE_URL = 'http://localhost:5002/api';
+const API_BASE_URL = 'http://134.209.50.92:5002/api';
 
 const elementService = {
   // CORRECCIÓN: Crear un nuevo elemento (agregando screenId al payload)
